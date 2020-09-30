92b252251130ac7d8ab274cc512deb304e92c67d
Валерий Гуров <ValeriyGourov@live.ru>
2020-09-30 20:57:23 +0300

    Добавлены классы для настроек приложения

---
+++ Downloader/Infrastructure/EngineSettingsValidationException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Downloader.Infrastructure
+{
+	/// <summary>
+	/// Исключение, возникающее при проверке настроек движка.
+	/// </summary>
+	[Serializable]
+	public sealed class EngineSettingsValidationException : Exception
+	{
+		/// <summary>
+		/// Перечень результатов проверки некорректных настроек.
+		/// </summary>
+		public IEnumerable<ValidationResult> ValidationResults { get; } = new List<ValidationResult>();
+
+		public EngineSettingsValidationException()
+		{
+		}
+
+		public EngineSettingsValidationException(
+			string message,
+			List<ValidationResult> validationResults)
+			: this(message, null, validationResults)
+		{
+		}
+
+		public EngineSettingsValidationException(
+			string message,
+			Exception innerException,
+			List<ValidationResult> validationResults)
+			: base(message, innerException)
+		{
+			ValidationResults = validationResults;
+		}
+	}
+}
---
+++ ItvdnCoursesDownloaderConsole/AppSettings.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using Downloader;
+
+namespace ItvdnCoursesDownloaderConsole
+{
+	/// <summary>
+	/// Настройки приложения.
+	/// </summary>
+	internal sealed class AppSettings
+	{
+		/// <summary>
+		/// Полный адрес курса на сайте.
+		/// </summary>
+		[Url]
+		public string CourseAddress { get; set; }
+
+		/// <summary>
+		/// Секция настроек движка загрузки файлов.
+		/// </summary>
+		[Required]
+		public EngineSettings Engine { get; set; }
+	}
+}
