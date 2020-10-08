00f7a92a888e8d2e68abcea460ffa7ad9dbd9a45
Валерий Гуров <ValeriyGourov@live.ru>
2020-10-08 18:25:46 +0300

    Класс FileSizeFormatHelper сделан статическим

--- Downloader/Utilities/FileSizeFormatHelper.cs
+++ Downloader/Utilities/FileSizeFormatHelper.cs
@@ -2,21 +2,21 @@
 
 namespace Downloader.Utilities
 {
-    /// <summary>
-    /// Вспомогательный класс для форматирования размера файла.
-    /// </summary>
-    public class FileSizeFormatHelper
-    {
-        /// <summary>
-        /// Преобразовывает числовое значение в строковое, которое представляет число, выраженное как значение размера в байтах, килобайтах, мегабайтах и т.д., в зависимости от размера.
-        /// </summary>
-        /// <param name="fileSize">Числовое значение размера файла для конвертации.</param>
-        /// <returns>Форматированное представление размера файла.</returns>
-        public static string FormatByteSize(long fileSize)
-        {
-            StringBuilder buffer = new StringBuilder(11);
-            NativeMethods.StrFormatByteSize(fileSize, buffer, buffer.Capacity);
-            return buffer.ToString();
-        }
-    }
+	/// <summary>
+	/// Вспомогательный класс для форматирования размера файла.
+	/// </summary>
+	public static class FileSizeFormatHelper
+	{
+		/// <summary>
+		/// Преобразовывает числовое значение в строковое, которое представляет число, выраженное как значение размера в байтах, килобайтах, мегабайтах и т.д., в зависимости от размера.
+		/// </summary>
+		/// <param name="fileSize">Числовое значение размера файла для конвертации.</param>
+		/// <returns>Форматированное представление размера файла.</returns>
+		public static string FormatByteSize(long fileSize)
+		{
+			StringBuilder buffer = new StringBuilder(11);
+			NativeMethods.StrFormatByteSize(fileSize, buffer, buffer.Capacity);
+			return buffer.ToString();
+		}
+	}
 }
